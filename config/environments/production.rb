d12c1b4a690265b6c8aad79c5e23d0ab52de478f
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-01-23 22:40:32 +0900

    fix4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,8 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.assets.compile = true
+  config.assets.initialize_on_precompile=false
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
